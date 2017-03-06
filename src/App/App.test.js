5ba2a6015dc29399579c1a50244a1ba692ea230e
Moses Yeung <yo_yeung@yahoo.com.hk>
2017-03-06 20:32:15 +0800

    First init

---
+++ src/App/App.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('render and have p have classname call App-intro', () => {
+  const app = shallow(<App />);
+  expect(app.find('.App-intro')).to.have.length(1);
+});
