3989f6a1e7949e24696d36f0da809eed64c52f33
Alex Lossing <lossing.alex@gmail.com>
2017-03-09 16:18:43 +0800

    docs(cicd): add comment in cicd script

--- cicd_scripts/cicd.sh
+++ cicd_scripts/cicd.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Launch th CI / CD pipeline :
+# - Build new release tag with CHANGELOG, npm-shrinkwrap and version bumping
+# - Commit CHANGELOG and version bumping (package.json version) in 'develop'
+# - Deploy to UAT
+# - Launch functional test
+# - Deploy to PROD
+# - Launch smoke
+
 set -e # Exit with nonzero exit code if anything fails
 echo "=================================================="
 echo "=============     STARTING CI/CD     ============="
@@ -11,10 +19,11 @@ bash ./cicd_scripts/build.sh
 # deploy in UAT
 bash ./cicd_scripts/deploy.sh ${UAT_S3_URI}
 
-# Launch integration/e@e2 testing
+# Launch function testing
 npm run functional-tests
 
 # deploy in PROD
 bash ./cicd_scripts/deploy.sh ${PROD_S3_URI}
 
+# TODO launch smock test
 
