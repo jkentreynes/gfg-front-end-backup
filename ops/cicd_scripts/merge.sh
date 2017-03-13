8798101770c6eadaee80f18ace8b766582e6fc8d
Alex Lossing <lossing.alex@gmail.com>
2017-03-13 12:19:23 +0800

    chore(cicd): merge script get current branch name

--- ops/cicd_scripts/merge.sh
+++ ops/cicd_scripts/merge.sh
@@ -25,16 +25,16 @@ else
     echo "MERGE_TO_BRANCH is set to '$MERGE_TO_BRANCH'"
 fi
 
-#TODO check if can work without modifying global credentials
+##TODO check if can work without modifying global credentials
 echo "== Setting GIT credentials =="
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
-echo "== Create new branch from current branch =="
-TMP_MERGE_FROM_BRANCH="merge-Travis-$BUILD_ID"
-git checkout -b "$TMP_MERGE_FROM_BRANCH"
+# Get current branch
+TMP_MERGE_FROM_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Current branch: $TMP_MERGE_FROM_BRANCH"
 
 echo "== Merging $MERGE_TO_BRANCH to $MERGE_TO_BRANCH =="
 # Merge branch, force change from MERGE_FROM_BRANCH (for solving conflict from npm-shrinkwrap)
