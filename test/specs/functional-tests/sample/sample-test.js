d37e7b7360412d51906ab78dfb9c7e9341e25778
John Kent Reynes <johnkentreynes@Johns-MacBook-Pro.local>
2017-03-22 14:57:48 +0800

    test(wdio): update smoke test

--- test/specs/functional-tests/sample/sample-test.js
+++ test/specs/functional-tests/sample/sample-test.js
@@ -3,7 +3,7 @@
 const samplePage = require('../../../page-objects/sample.page.js');
 
 describe('Sample Test', () => {
-  it('Should be at index page', () => {
+  it('Should be at sg page', () => {
     browser.url('/sg');
     expect(samplePage.isProductLogoVisible()).toBe(true);
   });
